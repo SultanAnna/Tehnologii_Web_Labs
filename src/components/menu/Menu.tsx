0e94f79464fc044d0ed78ce09035e12e7436afa4
ASultan <anna.sultan2002@gmail.com>
2023-05-11 14:31:58 +0300

    lab5

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,14 +1,14 @@
 import { inject, observer } from "mobx-react";
 import { Component } from "react";
-import { Layout, Menu , Card} from 'antd';
-import { Root } from "../../store/menu";
+import { Item, Root } from "../../store/menu";
 import 'antd/dist/reset.css';
 import {
   Button,
   Input,
   Form,
   message,
-  Table
+  Table,
+  Spin
 } from 'antd';
 
 interface MenuComponentProps {
@@ -19,7 +19,8 @@ interface MenuComponentState {
   itemName: string,
   itemTitle: string,
   itemDescription: string,
-  menuClicked: boolean
+  menuData: Item[],
+  loading: boolean
 }
 
 @inject("rootTree")
@@ -27,25 +28,39 @@ interface MenuComponentState {
 class MenuComponent extends Component<
   MenuComponentProps,
   MenuComponentState>
-  {
-
+{
   constructor(props: MenuComponentProps) {
     super(props);
     this.state = {
       itemName: '',
       itemTitle: '',
       itemDescription: '',
-      menuClicked: false
+      menuData: [],
+      loading: true
     };
   }
 
-  handleMenuClick = (e: any) =>{
-    const itemId = e.key;
-    if (itemId === "1") {
-      this.setState({ menuClicked: true });
-    } else {
-      this.setState({ menuClicked: false });
-    }
+  async componentDidMount() {
+    const { rootTree } = this.props;
+    if (!rootTree) return <Spin size="large" />;
+
+    this.setState({ loading: true });
+
+    const items = await this.getItemsFromLocalStorage();
+    rootTree.menu.setItems(items);
+    this.setState({ menuData: [...rootTree.menu.items] });
+
+    this.setState({ loading: false });
+  }
+
+  async getItemsFromLocalStorage(): Promise<Item[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const menu = localStorage.getItem('Menu');
+        const items = menu ? JSON.parse(menu) : [];
+        resolve(items);
+      }, 2000);
+    });
   }
 
   changeItemName = (e: any) => {
@@ -73,105 +88,107 @@ class MenuComponent extends Component<
 
     rootTree.menu.newItem(itemName, itemTitle, itemDescription);
 
+    this.setState({ menuData: [...rootTree.menu.items] });
+
+    localStorage.setItem('Menu', JSON.stringify([...rootTree.menu.items]));
+
     setTimeout(() => {
       message.success('Item was added');
     }, 300);
   }
 
   render() {
-      const { rootTree } = this.props;
-      const { itemName, itemTitle, itemDescription, menuClicked } = this.state;
+    const { rootTree } = this.props;
+    const { itemName, itemTitle, itemDescription } = this.state;
 
-      if (!rootTree) return null;
+    if (!rootTree) return null;
 
-      const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
-        },
-        {
-          title: 'Title',
-          dataIndex: 'title',
-          key: 'title',
-        },
-        {
-          title: 'Description',
-          dataIndex: 'description',
-          key: 'description',
-        }
-      ];
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'Title',
+        dataIndex: 'title',
+        key: 'title',
+      },
+      {
+        title: 'Description',
+        dataIndex: 'description',
+        key: 'description',
+      }
+    ];
 
-      const dataSource = rootTree.menu.items.map((item) => ({
-        id: item.id,
-        name: item.name,
-        title: item.title,
-        description: item.description
-      }));
+    const dataSource = this.state.menuData.map((item) => ({
+      id: item.id,
+      name: item.name,
+      title: item.title,
+      description: item.description
+    }));
 
-      return(
-        <div>
-          <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']} onClick={this.handleMenuClick}>
-            <Menu.Item key="1" title="menu">Menu</Menu.Item>
-            <Menu.Item key="2">About</Menu.Item>
-          </Menu>
-          {menuClicked ? (
-            <div>
-              <Form
-                color="red"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 16 }}
-                style={{ maxWidth: 600 }}>
-                <Form.Item
-                  label="Name"
-                  name="name">
-                  <Input
-                    allowClear
-                    placeholder="input name"
-                    value={itemName}
-                    onChange={this.changeItemName}>
-                  </Input>
-                </Form.Item>
-                <Form.Item
-                  label="Title"
-                  name="title">
-                  <Input
-                    allowClear
-                    placeholder="input title"
-                    value={itemTitle}
-                    onChange={this.changeItemTitle}>
-                  </Input>
-                </Form.Item>
-                <Form.Item
-                  label="Description"
-                  name="description">
-                  <Input
-                    allowClear
-                    placeholder="input description"
-                    value={itemDescription}
-                    onChange={this.changeItemDescription}>
-                  </Input>
-                </Form.Item>
-                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                  <Button
-                    block
-                    type='primary'
-                    htmlType="submit"
-                    onClick={ this.onSubmit }>
-                    Submit
-                  </Button>
-                </Form.Item>
-              </Form>
-              <Table dataSource={dataSource} columns={columns} rowKey="id"/>
-            </div>
-          ) : (
-            <div style={{ padding: '30px' }}>
-              <p>Some text about the company or the website.</p>
-            </div>
-          )}
-        </div>
-      );
-    }
+    return (
+      <div>
+        <Spin size="large" spinning={this.state.loading}></Spin>
+        <h1>Menu</h1>
+        <Form
+          color="red"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+        >
+          <Form.Item
+            label="Name"
+            name="name"
+          >
+            <Input
+              allowClear
+              placeholder="input name"
+              value={itemName}
+              onChange={this.changeItemName}
+            >
+            </Input>
+          </Form.Item>
+          <Form.Item
+            label="Title"
+            name="title"
+          >
+            <Input
+              allowClear
+              placeholder="input title"
+              value={itemTitle}
+              onChange={this.changeItemTitle}
+            >
+            </Input>
+          </Form.Item>
+          <Form.Item
+            label="Description"
+            name="description"
+          >
+            <Input
+              allowClear
+              placeholder="input description"
+              value={itemDescription}
+              onChange={this.changeItemDescription}
+            >
+            </Input>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button
+                block
+                type='primary'
+                htmlType="submit"
+                onClick={ this.onSubmit }
+              >
+                Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        <Table dataSource={dataSource} columns={columns} rowKey="id" />
+      </div>
+    );
   }
+}
 
-  export { MenuComponent };
+export { MenuComponent };
